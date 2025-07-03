96341162c2f8b1e7186d6ffb212a4ae606c00209
Raghu K <raghuk.vit@gmail.com>
2025-07-03 08:52:16 +0530

    RoboShop Code

--- roboshop.tf
+++ roboshop.tf
@@ -8,102 +8,17 @@ variable "instance_type" {
   default = "t3.small"
 }
 
-
-resource "aws_instance" "frontend" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "frontend"
-  }
+variable "instances" {
+  default = ["frontend", "catalogue" ,"cart", "user", "shipping", "payment", "mongodb", "redis", "rabbitmq", "mysql"]
 }
 
-resource "aws_instance" "mysql" {
+resource "aws_instance" "instance" {
+  count = length(var.instances)
   ami           = var.ami
   instance_type = var.instance_type
 
   tags = {
-    Name = "mysql"
-  }
-}
-
-
-resource "aws_instance" "mongodb" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-
-
-resource "aws_instance" "catalogue" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "catalogue"
-  }
-}
-
-
-resource "aws_instance" "redis" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "redis"
-  }
-}
-
-
-resource "aws_instance" "cart" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "cart"
-  }
-}
-
-
-resource "aws_instance" "user" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "user"
-  }
-}
-
-
-resource "aws_instance" "shipping" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "shipping"
-  }
-}
-
-
-resource "aws_instance" "rabbitmq" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "rabbitmq"
-  }
-}
-
-
-resource "aws_instance" "payment" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "payment"
+    Name = var.instances[count.index]
   }
 }
 
