191f1ddaa63eae547646035e03896b826adaaa29
Raghu K <raghuk.vit@gmail.com>
2025-07-08 07:59:25 +0530

    RoboShop Code

--- modules/ec2/locals.tf
+++ modules/ec2/locals.tf
@@ -1,5 +1,6 @@
 locals {
   tagName = var.env == null ? var.name : "${var.name}-${var.env}"
   dnsName = var.env == null ? "${var.name}-internal" : "${var.name}-${var.env}"
+  dnsNamePublic = var.env == null ? var.name : "${var.name}-${var.env}"
 }
 
