03d76cafcc4fc7e20088e51ba3951907f536cf2b
Raghu K <raghuk.vit@gmail.com>
2025-08-07 07:41:45 +0530

    RoboShop

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-# module "ec2" {
-#   for_each      = var.instances
-#   source        = "./modules/ec2"
-#   ami           = var.ami
-#   env           = var.env
-#   instance_type = each.value["instance_type"]
-#   disk_size     = each.value["disk_size"]
-#   name          = each.key
-#   zone_id       = var.zone_id
-#   token         = var.token
-# }
-#
-
-module "eks" {
-  source = "./modules/eks"
+module "ec2" {
+  for_each      = var.instances
+  source        = "./modules/ec2"
+  ami           = var.ami
+  env           = var.env
+  instance_type = each.value["instance_type"]
+  disk_size     = each.value["disk_size"]
+  name          = each.key
+  zone_id       = var.zone_id
+  token         = var.token
 }
+
+
+# module "eks" {
+#   source = "./modules/eks"
+# }
