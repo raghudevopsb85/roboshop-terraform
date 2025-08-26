6bdd9ff07e3671a6a081161bb807d5cc4d739a51
Raghu K <raghuk.vit@gmail.com>
2025-08-26 07:49:42 +0530

    RoboShop

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -1,4 +1,15 @@
+resource "null_resource" "kubeconfig" {
+
+  depends_on = [aws_eks_node_group.main]
+
+  provisioner "local-exec" {
+    command = "aws eks update-kube-config --name ${var.env}"
+  }
+
+}
+
 resource "helm_release" "nginx_ingress" {
+  depends_on = [null_resource.kubeconfig]
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
