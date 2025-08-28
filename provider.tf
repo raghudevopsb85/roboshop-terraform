1a6c40f68a61994978c582659327ac3c461a5919
Raghu K <raghuk.vit@gmail.com>
2025-08-28 08:17:50 +0530

    RoboShop

--- provider.tf
+++ provider.tf
@@ -3,6 +3,12 @@ provider "aws" {
 }
 terraform {
   backend "s3" {}
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "= 2.17.0"
+    }
+  }
 }
 
 provider "vault" {
