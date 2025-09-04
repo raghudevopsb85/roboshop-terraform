a4203a910a13fd25ef46e076d843b1cac61bd513
Raghu K <raghuk.vit@gmail.com>
2025-09-04 07:56:34 +0530

    RoboShop

--- helm-charts/iam.tf
+++ helm-charts/iam.tf
@@ -43,5 +43,54 @@ resource "aws_eks_pod_identity_association" "external-dns" {
   role_arn        = aws_iam_role.external-dns.arn
 }
 
+## Prometheus EC2 Discovery
+
+resource "aws_iam_role" "prometheus-server" {
+  name = "${var.env}-pod-prometheus-server-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "external-dns"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = [
+            "ec2:DescribeInstances",
+            "ec2:DescribeInstanceStatus"
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+
+}
+
+resource "aws_eks_pod_identity_association" "prometheus-server" {
+  cluster_name    = var.env
+  namespace       = "tools"
+  service_account = "kube-prometheus-stack-prometheus"
+  role_arn        = aws_iam_role.external-dns.arn
+}
+
+
 
 
