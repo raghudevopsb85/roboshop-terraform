b4c3fb241c86ee43f5adf5397225176233fbb46c
Raghu K <raghuk.vit@gmail.com>
2025-09-25 07:56:57 +0530

    RoboShop

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -42,3 +42,10 @@ resource "aws_route" "other-to-main" {
   vpc_peering_connection_id = aws_vpc_peering_connection.main[each.key].id
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = var.env
+  }
+}
