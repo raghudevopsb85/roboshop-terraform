342219fee5f553f24d43db6f2b58372bf584ab28
Raghu K <raghuk.vit@gmail.com>
2025-10-03 08:58:11 +0530

    RoboShop

--- helm-charts/alb.tf
+++ helm-charts/alb.tf
@@ -70,5 +70,20 @@ resource "aws_lb_listener" "https" {
   }
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
 
 
