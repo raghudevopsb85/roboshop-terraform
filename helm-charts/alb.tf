79d3e1517cba3a698beef612ec8d0334cf8e944e
Raghu K <raghuk.vit@gmail.com>
2025-10-03 08:54:51 +0530

    RoboShop

--- helm-charts/alb.tf
+++ helm-charts/alb.tf
@@ -58,6 +58,17 @@ resource "aws_lb_target_group_attachment" "tg-attach" {
   port             = 80
 }
 
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:us-east-1:739561048503:certificate/cb889901-6aad-4d52-ae3d-37429520d874"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
 
 
 
