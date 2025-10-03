fd3e6d809a27cd061e71fecac10035c2a57038d0
Raghu K <raghuk.vit@gmail.com>
2025-10-03 08:36:14 +0530

    RoboShop

--- helm-charts/alb.tf
+++ helm-charts/alb.tf
@@ -43,4 +43,19 @@ resource "aws_lb" "main" {
   }
 }
 
+resource "aws_lb_target_group" "tg" {
+  name     = "public-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.main.id
+  target_type = "ip"
+}
+
+resource "aws_lb_target_group_attachment" "tg-attach" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = "10.10.1.1"
+  port             = 80
+}
+
+
 
