73be75ffc48f2389cdfd8a34abfab64eeb74f270
Raghu K <raghuk.vit@gmail.com>
2025-10-10 08:20:28 +0530

    RoboShop

--- modules/ec2/iam.tf
+++ modules/ec2/iam.tf
@@ -37,3 +37,7 @@ resource "aws_iam_instance_profile" "main" {
   role = aws_iam_role.main.name
 }
 
+resource "aws_iam_role_policy_attachment" "inspector-scanning-attachment" {
+  role       = aws_iam_role.main.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
